6d144914ae836aa40f4a74dbf79c58ee53fe7e5c
adnansadar <adnansadar11@gmail.com>
2019-05-26 12:15:34 +0530

    made modifications

--- Sorting/src/com/company/insertion_sort.java
+++ Sorting/src/com/company/insertion_sort.java
@@ -4,21 +4,52 @@ public class insertion_sort {
     public static void main(String[] args) {
         int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
 
-        for (int firstUnsortedIndex = 1; firstUnsortedIndex < intArray.length;// as sorted index at start is always 0
-             firstUnsortedIndex++) {
-            int newElement = intArray[firstUnsortedIndex];
+        insertionSort(intArray, intArray.length);
 
-            int i;
-
-            for (i = firstUnsortedIndex; i > 0 && intArray[i - 1] > newElement; i--) {
-                intArray[i] = intArray[i - 1];
-            }
-
-            intArray[i] = newElement;
-        }
+//        for (int firstUnsortedIndex = 1; firstUnsortedIndex < intArray.length;
+//             firstUnsortedIndex++) {
+//            int newElement = intArray[firstUnsortedIndex];
+//
+//            int i;
+//
+//            for (i = firstUnsortedIndex; i > 0 && intArray[i - 1] > newElement; i--) {
+//                intArray[i] = intArray[i - 1];
+//            }
+//
+//            intArray[i] = newElement;
+//        }
 
         for (int i = 0; i < intArray.length; i++) {
             System.out.println(intArray[i]);
         }
+    }
+
+    public static void insertionSort(int[] input, int numItems) {
+
+        if (numItems < 2) {
+            return;
+        }
+
+        insertionSort(input, numItems - 1);
+
+        int newElement = input[numItems - 1];
+
+        int i;
+
+        for (i = numItems - 1; i > 0 && input[i - 1] > newElement; i--) {
+            input[i] = input[i - 1];
+        }
+
+        input[i] = newElement;
+
+        System.out.println("Result of call when numItems = " + numItems);
+        for (i = 0; i < input.length; i++) {
+            System.out.print(input[i]);
+            System.out.print(", ");
+        }
+        System.out.println("");
+        System.out.println("---------------------");
+
+
     }
 }
