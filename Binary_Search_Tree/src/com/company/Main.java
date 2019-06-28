60125f0425d25fe6505ab52ddbd8addbfcf8f44e
adnansadar <adnansadar11@gmail.com>
2019-06-28 12:06:15 +0530

    slight modifications in the code

--- Binary_Search_Tree/src/com/company/Main.java
+++ Binary_Search_Tree/src/com/company/Main.java
@@ -13,6 +13,17 @@ public class Main {
         intTree.insert(26);
         intTree.insert(22);
         intTree.insert(32);
+        intTree.insert(17);
 
+//        intTree.traverseInOrder();
+//        System.out.println();
+//
+//        System.out.println(intTree.get(27));
+//        System.out.println(intTree.get(17));
+//        System.out.println(intTree.get(8888));
+
+        System.out.println(intTree.min());
+        System.out.println(intTree.max());
     }
 }
+
--- Binary_Search_Tree/src/com/company/TreeNode.java
+++ Binary_Search_Tree/src/com/company/TreeNode.java
@@ -8,6 +8,10 @@ public class TreeNode {
     private TreeNode leftChild;
     private TreeNode rightChild;
 
+    public TreeNode(int data) {
+        this.data = data;
+    }
+
     public void insert(int value) {
         if (value == data) {
             return;
@@ -31,8 +35,51 @@ public class TreeNode {
         }
     }
 
-    public TreeNode(int data) {
-        this.data = data;
+    public TreeNode get(int value) {
+        if (value == data) {
+            return this;
+        }
+
+        if (value < data) {
+            if (leftChild != null) {
+                return leftChild.get(value);
+            }
+        }
+        else {
+            if (rightChild != null) {
+                return rightChild.get(value);
+            }
+        }
+
+        return null;
+    }
+
+    public int min() {
+        if (leftChild == null) {
+            return data;
+        }
+        else {
+            return leftChild.min();
+        }
+    }
+
+    public int max() {
+        if (rightChild == null) {
+            return data;
+        }
+        else {
+            return rightChild.max();
+        }
+    }
+
+    public void traverseInOrder() {
+        if (leftChild != null) {
+            leftChild.traverseInOrder();
+        }
+        System.out.print(data + ", ");
+        if (rightChild != null) {
+            rightChild.traverseInOrder();
+        }
     }
 
     public int getData() {
@@ -57,5 +104,9 @@ public class TreeNode {
 
     public void setRightChild(TreeNode rightChild) {
         this.rightChild = rightChild;
+    }
+
+    public String toString() {
+        return "Data = " + data;
     }
 }
