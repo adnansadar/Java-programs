5e5c0715b77f491d490e0b844332ed7be363ab6d
adnansadar <adnansadar11@gmail.com>
2019-07-20 11:11:57 +0530

    added comments

--- linked_list/src/com/company/Main.java
+++ linked_list/src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        //goes to employee class constructor
         Employee janeJones = new Employee("Jane", "Jones", 123);
         Employee johnDoe = new Employee("John", "Doe", 4567);
         Employee marySmith = new Employee("Mary", "Smith", 22);
